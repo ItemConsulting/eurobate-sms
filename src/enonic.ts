99449aa34c35fd19cf19f70c6effe04b35605522
Tom Arild Jakobsen <tajakobsen@gmail.com>
2019-10-30 11:17:06 +0100

    Update dependencies and bump to 0.1.0

--- src/enonic.ts
+++ src/enonic.ts
@@ -1,14 +1,22 @@
-import { Either, parseJSON, chain, filterOrElse } from "fp-ts/lib/Either";
-import { pipe} from "fp-ts/lib/pipeable";
-import { Error } from 'enonic-fp/lib/common';
+import { parseJSON } from "fp-ts/lib/Either";
+import { IOEither, fromEither, chain, filterOrElse } from "fp-ts/lib/IOEither";
+import { pipe } from "fp-ts/lib/pipeable";
+import { EnonicError } from 'enonic-fp/lib/common';
 import { request, HttpRequestParams, HttpResponse } from 'enonic-fp/lib/http';
 import { EurobateParams, EurobateResponse } from "./index";
 
-export function json(str: string) : Either<Error, any> {
-  return parseJSON<Error>(str, reason => ({ errorKey: "BadRequestError", cause: String(reason) }));
+export function json(str: string) : IOEither<EnonicError, any> {
+  return fromEither(
+    parseJSON<EnonicError>(str, (reason) => (
+      {
+        errorKey: "BadGatewayError",
+        cause: String(reason)
+      })
+    )
+  );
 }
 
-export function sendSMS(params: EurobateParams): Either<Error, EurobateResponse> {
+export function sendSMS(params: EurobateParams): IOEither<EnonicError, EurobateResponse> {
   const requestParams : HttpRequestParams = {
     url: 'https://api.eurobate.com/json_api.php',
     method: 'POST',
@@ -20,10 +28,15 @@ export function sendSMS(params: EurobateParams): Either<Error, EurobateResponse>
 
   return pipe(
     request(requestParams),
-    chain((res: HttpResponse) => json(res.body)),
+    chain((res: HttpResponse) => json(res.body!)),
     filterOrElse(
       (res: EurobateResponse) => res.STATUS !== 'ERROR',
-      e => ({ errorKey: "BadGatewayError", cause: String(e) }) as Error
+      e => (
+        {
+          errorKey: "BadGatewayError",
+          cause: String(e)
+        }
+      ) as EnonicError
     )
   );
 }
